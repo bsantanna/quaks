0111351d0b0c4bb55cf1fdbb5832e7284c4dec05
Bruno Sant'Anna <mail@bsantanna.me>
2025-12-10 23:31:04 +0100

    fix: get news endpoint (3)

--- app/interface/api/markets/endpoints.py
+++ app/interface/api/markets/endpoints.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 
 from app.core.container import Container
 from app.interface.api.cache_control import cache_control
-from app.interface.api.markets.schema import StatsClose, StatsCloseRequest, NewsItem
+from app.interface.api.markets.schema import StatsClose, StatsCloseRequest, NewsItem, NewsList, NewsListRequest
 from app.services.markets_news import MarketsNewsService
 from app.services.markets_stats import MarketsStatsService
 
@@ -40,27 +40,44 @@ async def get_most_recent_close(
 
 
 @router.get(
-    path="/news_related/{index_name}/{key_ticker}",
-    response_model=list[NewsItem],
+    path="/news/{index_name}",
+    response_model=NewsList,
     operation_id="news_related",
     summary="Get news related to a given ticker"
 )
 @inject
-async def get_news_related(
+async def get_news(
         index_name: str,
-        key_ticker: str,
         markets_news_service: MarketsNewsService = Depends(Provide[Container.markets_news_service]),
-        _=cache_control(3600)
-):
-    response = []
-    result = await markets_news_service.get_news_related(index_name, key_ticker)
+        request: NewsListRequest = Depends(),
+        _=cache_control(3600)):
+
+    result, sort = await markets_news_service.get_news(
+        index_name=index_name,
+        key_ticker=request.key_ticker,
+        size=request.size,
+        cursor=request.cursor,
+        include_text_content=request.include_text_content,
+        include_key_ticker=request.include_key_ticker,
+        include_obj_images=request.include_obj_images,
+    )
+
+    news_items = []
     for item in result:
-        response.append(NewsItem(
+        news_items.append(NewsItem(
             url=item.get("key_url"),
             source=item.get("key_source"),
             headline=item.get("text_headline"),
             summary=item.get("text_summary"),
+            content=item.get("text_content"),
             date=item.get("date_reference"),
+            images=item.get("obj_images"),
+            key_ticker=item.get("key_ticker"),
         ))
+
+    response = NewsList(
+        items=news_items,
+        cursor=sort
+    )
 
     return response
--- app/services/markets_news.py
+++ app/services/markets_news.py
@@ -1,3 +1,5 @@
+import base64
+import json
 from elasticsearch import Elasticsearch
 
 
@@ -6,27 +8,32 @@ class MarketsNewsService:
     def __init__(self, es: Elasticsearch) -> None:
         self.es = es
 
-    async def get_news_related(
+    async def get_news(
             self,
             index_name: str,
-            key_ticker: str,
+            key_ticker: str = None,
             size=10,
+            cursor: str = None,
+            include_text_content=False,
             include_key_ticker=False,
-            include_images=False
-    ) -> list[dict]:
+            include_obj_images=False):
         search_params = {
-            "id": "get_markets_news_related_template",
+            "id": "get_markets_news_template",
             "params": {
                 "key_ticker": key_ticker,
                 "size": size,
+                "include_text_content": include_text_content,
                 "include_key_ticker": include_key_ticker,
-                "include_images": include_images
+                "include_obj_images": include_obj_images,
+                "search_after": json.loads(base64.urlsafe_b64decode(cursor).decode()) if cursor else None,
             }
         }
 
         response = self.es.search_template(index=index_name, body=search_params)
         results = []
+        cursor = None
         for hit in response['hits']['hits']:
             results.append(hit['_source'])
+            cursor = base64.urlsafe_b64encode(json.dumps(hit['sort']).encode()).decode()
 
-        return results
+        return results, cursor
