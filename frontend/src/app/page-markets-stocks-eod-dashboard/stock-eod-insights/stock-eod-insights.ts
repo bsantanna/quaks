9cd962e774b32d663122c38b519e842c847d18bc
Bruno Sant'Anna <mail@bsantanna.me>
2025-12-11 20:30:36 +0100

    fix: prototypes action panel for stocks eod dashboard

---
+++ frontend/src/app/page-markets-stocks-eod-dashboard/stock-eod-insights/stock-eod-insights.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-eod-insights',
+  imports: [],
+  templateUrl: './stock-eod-insights.html',
+  styleUrl: './stock-eod-insights.scss',
+})
+export class StockEodInsights {
+
+}
---
+++ frontend/src/app/page-markets-stocks-eod-dashboard/stock-eod-tools/stock-eod-tools.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-eod-tools',
+  imports: [],
+  templateUrl: './stock-eod-tools.html',
+  styleUrl: './stock-eod-tools.scss',
+})
+export class StockEodTools {
+
+}
