bf7fc005ace0986bee79f3e8ab09f63d381a9827
Bruno Sant'Anna <mail@bsantanna.me>
2026-02-23 23:29:05 +0100

    fix: frontend layout 8

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
 };
 
 export default config;
