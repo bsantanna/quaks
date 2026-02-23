ad051debe97444af64b0b444cfea6fddd561aeb6
Bruno Sant'Anna <mail@bsantanna.me>
2026-02-23 23:50:51 +0100

    fix: sonar fixes 1

--- frontend/src/app/shared/components/stock-info-header/stock-info-header.ts
+++ frontend/src/app/shared/components/stock-info-header/stock-info-header.ts
@@ -1,6 +1,5 @@
-import {Component, computed, effect, inject, input, model, Signal, signal, WritableSignal} from '@angular/core';
+import {Component, computed, effect, inject, input, model, Signal} from '@angular/core';
 import {MarketsStatsService} from '../../services/markets-stats.service';
-import {StatsClose} from '../../models/markets.model';
 import {DecimalPipe} from '@angular/common';
 import {ShareUrlService} from '../../services/share-url.service';
 import {take} from 'rxjs';
