57b13f70d6332644b9d5ed866b74721a46ed1079
Bruno Sant'Anna <mail@bsantanna.me>
2026-03-05 22:25:12 +0100

    fix: version adjust

--- terraform/02_airflow-instance/variables.tf
+++ terraform/02_airflow-instance/variables.tf
@@ -12,7 +12,7 @@ variable "airflow_fqdn" {
 variable "quaks_dags_image_tag" {
   description = "Docker image tag for quaks-dags"
   type        = string
-  default     = "v1.3.25"
+  default     = "v1.3.26"
 }
 
 variable "airflow_admin_username" {
