1c880b09e69437e9b351940205f709a2f4976b14
Bruno Sant'Anna <mail@bsantanna.me>
2026-03-10 11:33:38 +0100

    fix: prototypes insights (1)

---
+++ frontend/src/app/page-insights-agents/insights-agents.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insights-agents',
+  imports: [],
+  templateUrl: './insights-agents.html',
+  styleUrl: './insights-agents.scss',
+})
+export class InsightsAgents {
+
+}
