da81ff752e06e8912004b716ef74e800cd13c5e6
Bruno Sant'Anna <mail@bsantanna.me>
2026-03-10 12:36:20 +0100

    fix: prototypes insights (5)

--- frontend/src/app/page-insights-profile/insights-profile.spec.ts
+++ frontend/src/app/page-insights-profile/insights-profile.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { InsightsProfile } from './insights-profile';
 
@@ -8,9 +11,13 @@ describe('InsightsProfile', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [InsightsProfile]
-    })
-    .compileComponents();
+      imports: [InsightsProfile],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([])
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InsightsProfile);
     component = fixture.componentInstance;
